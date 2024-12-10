2c2dea445d3764055ba29960f9bf390b467dfc2a
경아 <sun4354@naver.com>
2024-12-10 14:06:31 +0900

    Add files via upload

--- js/setting.js
+++ js/setting.js
@@ -16,7 +16,19 @@ $('.content_link a').click(function () {
 });
 
 /* 지면 사이즈 선택 */
-$('.form_box_wrap button').click(function () {
-	$('.form_box_wrap button').removeClass('active');
+$('.form_box_wrapper1 .form_box_wrap button').click(function () {
+	$('.form_box_wrapper1').find($('.form_box_wrap button')).removeClass('active');
+	$(this).addClass('active');
+	$('.form_box_wrapper2').css('display', 'flex');
+
+	/* 처음 등록 시 .new 클래스 화면 예시*/
+	if ($(this).parents('.form_box_wrapper1').hasClass('new')) {
+		$('.form_box_wrapper2 .form_box').css('display', 'none');
+		$('.form_box_wrapper2 .form_box_none').css('display', 'flex');
+	}
+});
+
+$('.form_box_wrapper2 .form_box_wrap button').click(function () {
+	$('.form_box_wrapper2').find($('.form_box_wrap button')).removeClass('active');
 	$(this).addClass('active');
 });
